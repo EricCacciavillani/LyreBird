b945c17da20c5f9836c056f821d15dec91384a51
EricCacciavillani <e1m2c4@yahoo.com>
2018-12-19 17:08:22 -0500

    Fixed shelf serialization bugs! And model for focal loss and focal loss fixed are saving correctly now. Commenting and formating is a lot nicer curretnly. Going to start work on multithreaded operations,styling up the output, and final project dir arrangemnt

--- Pre_Production/LSTM_Generation/Catgorical_Note_Model_Generation/Categorical_Notes_Model_Train.py
+++ Pre_Production/LSTM_Generation/Catgorical_Note_Model_Generation/Categorical_Notes_Model_Train.py
@@ -1,182 +1,188 @@
-from music21 import converter, note, chord
-import glob
-import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout, Activation
 from keras.optimizers import RMSprop
+from music21 import converter, note, chord
 from random import shuffle
 from collections import defaultdict
+from keras.models import load_model
+import numpy as np
 import sys
 import os
-from keras.models import load_model
+import glob
 import shelve
+import time
 
+# Import shared files
 sys.path.insert(0, '/home/eric/Desktop/LyreBird/Shared_Files')
 from Global_Util import *
 from Constants import *
 from Focal_Loss import *
 
-# --- Preprocessing
-# Grab notes and cords for training our model to predict.
-
+# Create constants
 TRACK_INDEX = 0
 
 TRAINING_DATASET_PTH = sys.argv[1]
 
 GENRE_NAME = (TRAINING_DATASET_PTH.split('/', -1)[-1]).replace('_Midi','')
 
+# Grab files full path for each midi file in the training set
 train_files = [TRAINING_DATASET_PTH + "/" + file
                for file in glob.glob1(TRAINING_DATASET_PTH, "*.mid")]
 
+TOTAL_TRAIN_FILES_LEN = len(train_files)
 
+# Empty directory check
 if not train_files:
     exception_output \
         = "ERROR: No Midi files were extracted from the following directory:",\
            TRAINING_DATASET_PTH
     raise Exception(exception_output)
 
+# We shuffle the entire set so each training batch for every genre varies
 shuffle(train_files)
+song_count = 0
 
+# Master loop; keep iterating until no files for that genre are left
 while train_files:
 
-    genre_notes = []
+    sampled_genre_notes = []
     for i, file in enumerate(train_files):
 
+        # Removing files already seen so the next genre sample will be
+        train_files.remove(file)
+
+        # ---
         try:
-            midi = converter.parse(file)
+            midi_song = converter.parse(file)
         except:
             continue
 
+        song_count += 1
+
         # Use the first track on the midi file (Multiple ones)
-        midi = midi[TRACK_INDEX]
+        midi_song = midi_song[TRACK_INDEX]
 
-        # Parse the midi file by the notes it contains
-        notes_to_parse = midi.flat.notes
+        # Extract notes from the song
+        notes_to_parse = midi_song.flat.notes
 
-        for element in notes_to_parse:
+        # Convert and append notes to our list
+        for note_element in notes_to_parse:
 
-            if isinstance(element, note.Note):
-                genre_notes.append(str(element.pitch))
-            elif isinstance(element, chord.Chord):
-                # Get the numerical representation
-                genre_notes.append('.'.join(str(n) for n in element.normalOrder))
+            if isinstance(note_element, note.Note):
+                sampled_genre_notes.append(str(note_element.pitch))
 
+            elif isinstance(note_element, chord.Chord):
+
+                # Get the numerical representation of the cord
+                sampled_genre_notes.append('.'.join(str(n)
+                                                    for n in note_element.normalOrder))
+
+        # Display song stats
         print("*" * 50)
         print("Song_Count: {0}:\nGenre_name: {1}\nFile_name: {2}\nNote_Count: {3}".format(
             i+1,GENRE_NAME,file.split('/', -1)[-1],
             len(notes_to_parse)))
         print("*" * 50)
 
-        train_files.remove(file)
-
-        if len(genre_notes) >= MAX_TRAINING_NOTES:
+        # Break if we get to many notes for our sub-sample
+        if len(sampled_genre_notes) >= MAX_TRAINING_NOTES:
             break
 
     print("DONE LOADING SONGS")
 
-    # Get all pitch names
+    # Un-Shelve data from file on directory path for encoded data
     label_encoder_shelve = shelve.open(LSTM_DATA_DUMP_PATH + 'Label_Encoder_Tone_Data')
     pitches = label_encoder_shelve["all_possible_tones"]
     length_all_possible_tones = label_encoder_shelve["length_all_possible_tones"]
-    master_note_decoder = label_encoder_shelve["master_note_decoder"]
     master_note_encoder = label_encoder_shelve["master_note_encoder"]
     label_encoder_shelve.close()
 
-    # Get all pitch names
-    vocab_length = length_all_possible_tones
-    number_notes = len(genre_notes)
+    # Pre-processing on data
+    num_training = len(sampled_genre_notes) - INPUT_SEQUENCE_LEN
+    input_notes = np.zeros((num_training, INPUT_SEQUENCE_LEN, length_all_possible_tones))
+    output_notes = np.zeros((num_training, length_all_possible_tones))
 
-    # Now we must get these notes in a usable form for our LSTM. Let's construct sequences that can be grouped together to predict the next note in groups of 10 notes.
-
-    # Let's use One Hot Encoding for each of the notes and create an array as such of sequences.
-    #Let's first assign an index to each of the possible notes
-
-    #print(note_dict)
-
-    # Now let's construct sequences. Taking each note and encoding it as a numpy array with a 1 in the position of the note it has
-    sequence_length = 50
-
-    # Lets make a numpy array with the number of training examples, sequence length, and the length of the one-hot-encoding
-    num_training = number_notes - sequence_length
-
-    input_notes = np.zeros((num_training, sequence_length, vocab_length))
-    output_notes = np.zeros((num_training, vocab_length))
-
+    # Set input and output for neural network;
+    # Using the encoder to one hot encode each tone in a set positional
+    # Input: Sequence of tones/notes found within sampled notes
+    # Output: A single note
+    # Summary: Predict a note with a given sequence
     for i in range(0, num_training):
-        # Here, i is the training example, j is the note in the sequence for a specific training example
-        input_sequence = genre_notes[i: i+sequence_length]
-        output_note = genre_notes[i+sequence_length]
+        input_sequence = sampled_genre_notes[i: i + INPUT_SEQUENCE_LEN]
+        output_note = sampled_genre_notes[i + INPUT_SEQUENCE_LEN]
         for j, tone in enumerate(input_sequence):
             input_notes[i][j][master_note_encoder[tone]] = 1
         output_notes[i][master_note_encoder[output_note]] = 1
 
-
+    # Model generation
     model = None
-    print(LSTM_MODELS_PATH + "Categorical_LSTM_Model.h5")
 
+    focal_loss_alpha = .25
+    foca_loss_gamma = 2.
+
+    # Check if the model already exists
     if os.path.isfile(LSTM_MODELS_PATH + "Categorical_LSTM_Model.h5"):
-        model = load_model((LSTM_MODELS_PATH + "Categorical_LSTM_Model.h5"))
+        model = load_model((LSTM_MODELS_PATH + "Categorical_LSTM_Model.h5"),
+                           custom_objects={'FocalLoss': focal_loss,
+                                           'focal_loss_fixed': focal_loss(alpha=focal_loss_alpha,
+                                                                          gamma=foca_loss_gamma)})
+
+    # Model doesn't exists currently; generate it
     else:
         model = Sequential()
-        model.add(LSTM(128, return_sequences=True, input_shape=(sequence_length,
-                                                                vocab_length)))
+        model.add(LSTM(128, return_sequences=True, input_shape=(INPUT_SEQUENCE_LEN,
+                                                                length_all_possible_tones)))
         model.add(Dropout(0.23))
         model.add(LSTM(128, return_sequences=False))
         model.add(Dropout(0.23))
-        model.add(Dense(vocab_length))
+        model.add(Dense(length_all_possible_tones))
         model.add(Activation('softmax'))
 
-        # [focal_loss(alpha=.25, gamma=2)]
-        model.compile(loss="categorical_crossentropy",
+        model.compile(loss=[focal_loss(alpha=focal_loss_alpha,
+                                       gamma=foca_loss_gamma)],
                       optimizer=RMSprop(lr=0.001),
                       metrics=['acc'])
     history = model.fit(input_notes, output_notes,
-                        batch_size=3000, nb_epoch=2)
-
-
-    #### Visualizing the Model's Results
-    # The models accuracy can be seen here increasing, as it learns the sequences over the course of 200 epochs.
-    # summarize history for accuracy
-
-    # ### Generating New Music
-
-    # Make a dictionary going backwards (with index as key and the note as the value)
+                        batch_size=int(MAX_TRAINING_NOTES/2),
+                        epochs=2)
 
     max_accur = max(history.history['acc']) * 100
     min_accur = min(history.history['acc']) * 100
     accur_growth = max_accur - min_accur
 
-    # model.save(LSTM_MODELS_PATH + "Categorical_LSTM_Model.h5")
+    # Save model on absolute path
+    model.save(LSTM_MODELS_PATH + "Categorical_LSTM_Model.h5")
+
+    # Send SMS to personal phone to keep updates
     # send_sms_to_me("{0} sub sampled data."
     #                "\nStarting Accuracy {1:.2f}%"
     #                "\nFinished Accuracy {2:.2f}%"
     #                "\nOverall Growth:{3:.2f}%".format(
     #     GENRE_NAME, min_accur, max_accur, accur_growth))
 
+    # Store data on model accuracy on set genres for later visualization
     model_accur_shelf = shelve.open(LSTM_DATA_DUMP_PATH +
-                                    '/Categorical_LSTM_Model_Accuracy')
+                                    'Categorical_LSTM_Model_Accuracy')
 
-
+    # Variable has yet to be stored yet ever before
     if "model_accur_by_genre" not in model_accur_shelf.keys():
         model_accur_shelf["model_accur_by_genre"] = defaultdict(dict)
 
-    if GENRE_NAME not in model_accur_shelf["model_accur_by_genre"].keys():
-        model_accur_shelf["model_accur_by_genre"][GENRE_NAME] = "Testing shit!!!!!"
-        print("jnjnjj")
+    model_accur_dict = model_accur_shelf["model_accur_by_genre"]
 
+    # Init new genre if needed
+    if GENRE_NAME not in model_accur_dict.keys():
+        model_accur_dict[GENRE_NAME] = {"max_accur": list(),
+                                        "min_accur": list(),
+                                        "accur_growth": list()}
 
-    print("Catch:")
-    print(model_accur_shelf["model_accur_by_genre"][GENRE_NAME])
-    print("End:")
-    # model_accur_shelf["model_accur_by_genre"][GENRE_NAME]["max_accur"].append(
-    #     max_accur)
-    #
-    # model_accur_shelf["model_accur_by_genre"][GENRE_NAME]["min_accur"].append(
-    #     min_accur)
-    #
-    # model_accur_shelf["model_accur_by_genre"][GENRE_NAME]["accur_growth"].append(
-    #     accur_growth)
+    model_accur_dict[GENRE_NAME]["max_accur"].append(max_accur)
+    model_accur_dict[GENRE_NAME]["min_accur"].append(min_accur)
+    model_accur_dict[GENRE_NAME]["accur_growth"].append(accur_growth)
+    model_accur_shelf["model_accur_by_genre"] = model_accur_dict
 
     model_accur_shelf.close()
 
-    print(len(train_files))
+    # Let the GPU cool per cycle
+    print("Let the GPU cool off...")
+    # time.sleep(SECONDS_TO_COOL_GPU)
--- Pre_Production/LSTM_Generation/Trainer_Controller.py
+++ Pre_Production/LSTM_Generation/Trainer_Controller.py
@@ -1,15 +1,16 @@
 import os
 import sys
-import time
+
 import shelve
 
+# Import shared files
 sys.path.insert(0, '/home/eric/Desktop/LyreBird/Shared_Files')
 from Constants import *
+
 DATASET_DIRECTORY_PATH = "/home/eric/Desktop/LyreBird/Datasets"
 DATASETS_PATHS = [x[0] for x in os.walk(DATASET_DIRECTORY_PATH)]
 DATASETS_PATHS.pop(0)
 
-exit(-1)
 i = 0
 for dataset_pth in DATASETS_PATHS:
     i += 1
@@ -17,6 +18,3 @@ for dataset_pth in DATASETS_PATHS:
         os.system('python ' + CATEGORICAL_MODEL_GENERATION
                   + 'Categorical_Notes_Model_Train.py'
                   + ' ' + dataset_pth)
-
-    print("Let the GPU cool off...")
-    time.sleep(SECONDS_TO_COOL_GPU)
