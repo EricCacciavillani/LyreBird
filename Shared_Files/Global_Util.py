65af2bfe8b032ee7cbaf81e7e558baa4f4772a74
EricCacciavillani <e1m2c4@yahoo.com>
2018-12-20 21:48:16 -0500

    New struc

--- Shared_Files/Global_Util.py
+++ Shared_Files/Global_Util.py
@@ -11,6 +11,3 @@ def send_sms_to_me(message="You forget to add a messag!"):
     client.messages.create(from_=os.environ.get('TWILIO_PHONE_NUMBER'),
                            to=os.environ.get('MY_PHONE_NUMBER'),
                            body=message)
-
-def extract_genre_name_from_dir(dir_str):
-    return (dir_str.split('/', -1)[-1]).replace('_Midi', '')
