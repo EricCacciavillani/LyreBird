0e7867c1b331e8026939eb3d2d210a6d233bedda
EricCacciavillani <e1m2c4@yahoo.com>
2018-12-22 03:02:15 -0500

    Finished commenting. Just need to finish train_test_split on label encoder and overall midi sequence attributes.

--- Pre_Production/Midi_Pre_Processor.py
+++ Pre_Production/Midi_Pre_Processor.py
@@ -1,8 +1,9 @@
 from music21 import converter, note, chord
+from music21.midi import MidiException
 import glob
-import numpy as np
 
 import os
+import copy
 from collections import Counter
 from multiprocessing.dummy import Pool as ThreadPool
 from tqdm import tqdm
@@ -69,11 +70,14 @@ class Midi_Pre_Processor:
             print('There were corrupted files found in your training.')
             display_options_menu(menu_intro="Corrupted files found! "
                                             "It is suggested to get rid of them",
-                                 menu_options={1: "Delete all corrupted.",
+                                 menu_options={1: "Delete all corrupted",
                                                2: "List all corrupted files",
                                                3: "Ignore"})
 
-            user_input = input("\nInput")
+            user_input = input("\nInput: ")
+            print(user_input)
+            print(type(user_input))
+            user_input = int(user_input)
 
             # Remove corrupted files
             if user_input == 1:
@@ -84,7 +88,8 @@ class Midi_Pre_Processor:
 
                 print("Displaying all corrupted songs:\n")
                 for song in self.__found_corrupted_file_paths:
-                    print(song.split("/", 7)[-1])
+                    print("\t",song.split("/", 7)[-1])
+                print()
             else:
                 pass
 
@@ -97,34 +102,32 @@ class Midi_Pre_Processor:
         self.__master_note_decoder = {v: k for k, v
                                       in self.__master_note_encoder.items()}
 
-        # Find files for best fit the value requirement
+        # Find files for best fit the for the validation set per genre
         for genre_name, note_counter in self.__all_genre_note_counters.items():
 
             genre_note_count = sum(note_counter.values())
-            needed_valadation_note_count = int((genre_note_count / self.__total_note_count)\
+            needed_validation_note_count = int((genre_note_count / self.__total_note_count)\
                                     * genre_note_count)
 
             note_count_file_dict = {v: k for k, v
                                     in self.__all_genre_file_note_count_dict[genre_name].items()}
 
             note_count_file_list = list(note_count_file_dict.keys())
-
-            while True and needed_valadation_note_count > 25:
+            '''
+            The validation count is decreasing per file note count;
+            When it reaches this arbitrary threshold the validation
+            set for this particular genre has been reached
+            '''
+            while True and needed_validation_note_count > 25:
 
                 closest_note_count = find_nearest(numbers=note_count_file_list,
-                                                  target=needed_valadation_note_count)
-                needed_valadation_note_count -= closest_note_count
+                                                  target=needed_validation_note_count)
+                needed_validation_note_count -= closest_note_count
 
                 self.__blacklisted_files_validation.add(
                     note_count_file_dict[closest_note_count])
 
                 note_count_file_list.remove(closest_note_count)
-
-            if not self.__blacklisted_files_validation:
-                self.__blacklisted_files_validation.add(note_count_file_dict[find_nearest(note_count_file_list, needed_valadation_note_count)])
-
-
-        print(self.__blacklisted_files_validation)
 
     # Displays core attributes
     def display_attributes(self):
@@ -140,32 +143,56 @@ class Midi_Pre_Processor:
         print("\nblacklist_files_validation: ", self.__blacklisted_files_validation)
         print("*" * 50)
 
-    # Threads out each dataset by it's genre
-    def __thread_pool_datasets_reader(self, function,
+    # Returns copied instances of the following attributes
+    def return_core_atributes(self):
+        return {"all_possible_notes": copy.deepcopy(
+            self.__all_possible_notes),
+            "all_possible_notes_counter": copy.deepcopy(
+                self.__all_possible_notes_counter),
+            "blacklisted_files_validation": copy.deepcopy(
+                self.__blacklisted_files_validation),
+            "all_genre_file_note_count_dict": copy.deepcopy(
+                self.__all_genre_file_note_count_dict),
+            "all_genre_note_counters": copy.deepcopy(
+                self.__all_genre_note_counters),
+            "found_corrupted_file_paths": copy.deepcopy(
+                self.__found_corrupted_file_paths),
+            "total_file_count": copy.deepcopy(self.__total_file_count),
+            "total_note_count": copy.deepcopy(self.__total_note_count),
+            "master_note_decoder": copy.deepcopy(self.__master_note_decoder),
+            "master_note_encoder": copy.deepcopy(self.__master_note_encoder)}
+
+    # Thread pools out the dataset by genre
+    def __thread_pool_datasets_reader(self, func,
                                       path_to_full_data_set):
 
-        # Get
+        # Get all folder paths for each genre
         all_train_datasets_paths = [x[0] for x in os.walk(
             path_to_full_data_set)]
         all_train_datasets_paths.pop(0)
 
-        files_by_genre = []
+        all_files_by_genre = []
 
         for dataset_pth in all_train_datasets_paths[:4]:
 
             dataset_files = [dataset_pth + "/" + file for file in
                              glob.glob1(dataset_pth, "*.mid")][:6]
 
+            # Ensures files were actually extracted
             if len(dataset_files):
                 self.__total_file_count += len(dataset_files)
 
-                files_by_genre.append(dataset_files)
+                all_files_by_genre.append(dataset_files)
 
+        # Init progress bar
         self.__pbar = tqdm(total=self.__total_file_count)
-        pool = ThreadPool(len(files_by_genre))
-        all_results = pool.imap_unordered(function,
-                                          files_by_genre)
 
+        # Begin threaded pool
+        pool = ThreadPool(len(all_files_by_genre))
+        all_results = pool.imap_unordered(func,
+                                          all_files_by_genre)
+
+        # End threaded pool
         pool.close()
         pool.join()
         self.__pbar.close()
@@ -173,15 +200,15 @@ class Midi_Pre_Processor:
 
         return all_results
 
-    def read_midi_file(self, file):
+    def read_midi_file_by_note(self, file,
+                               label_encode=False):
 
         song_notes = []
 
         # Attempt to parse midi file
         try:
             midi = converter.parse(file)
-        except Midi_Reading_Error as err:
-
+        except MidiException:
             # Midi file couldn't be opened
             return {"song_notes": [],
                     "note_count": [],
@@ -192,7 +219,6 @@ class Midi_Pre_Processor:
 
         # Parse the midi file by the notes it contains
         notes_to_parse = midi.flat.notes
-
         note_count = 0
 
         # Convert and append notes to our list; store tone count
@@ -200,6 +226,10 @@ class Midi_Pre_Processor:
 
             if isinstance(element, note.Note):
                 tone = str(element.pitch)
+
+                if label_encode and self.__master_note_encoder:
+                    tone = self.__master_note_encoder[tone]
+
                 song_notes.append(tone)
 
                 note_count += 1
@@ -208,6 +238,10 @@ class Midi_Pre_Processor:
 
                 # Get the numerical representation
                 tone = '.'.join(str(n) for n in element.normalOrder)
+
+                if label_encode and self.__master_note_encoder:
+                    tone = self.__master_note_encoder[tone]
+
                 song_notes.append(tone)
 
                 note_count += 1
@@ -216,6 +250,7 @@ class Midi_Pre_Processor:
                 "note_count": note_count,
                 "corrupted": False}
 
+    # Init full dataset attributes on object initialization
     def __genre_dataset_init(self, genre_train_files):
 
         all_genre_notes = []
@@ -227,8 +262,9 @@ class Midi_Pre_Processor:
         for _, file in enumerate(genre_train_files):
 
             self.__pbar.update(1)
-            self.__pbar.set_postfix_str(s=file.split("/",-1)[-1][:20], refresh=True)
-            midi_file_attr = self.read_midi_file(file)
+            self.__pbar.set_postfix_str(s=file.split("/",-1)[-1][:20],
+                                        refresh=True)
+            midi_file_attr = self.read_midi_file_by_note(file)
 
             if not midi_file_attr["corrupted"]:
                 file_note_count_dict[file] = midi_file_attr["note_count"]
@@ -243,5 +279,6 @@ class Midi_Pre_Processor:
                 "total_genre_notes": len(all_genre_notes)}
 
 
-
 a = Midi_Pre_Processor(ABS_PATHS.TRAINING_DATASET_DIRECTORY_PATH)
+
+a.display_attributes()
